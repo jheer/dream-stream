fd7e6b882f824bc971d54f47a993a579ba7311b0
Steven Drucker <stevendrucker@Steven-Druckers-MacBook-Pro.local>
2012-05-05 13:32:32 -0700

    First version of player

---
+++ player/examplejson.js
@@ -0,0 +1,3 @@
+var mydata = [ {text: "the rain in spain", image: "images/Image1.jpg", start: 0},
+	       {text: "falls mainly", image: "images/Image2.jpg", start: 2000},
+	       {text: "in the plain", image: "images/Image3.jpg", start: 3000}];
